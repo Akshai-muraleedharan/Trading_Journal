f2e46d925d64d12dd50b14445a3aa9c4fa2eaa6e
Akshai-muraleedharan <akshaimuraleedharan4@gmail.com>
2026-03-28 21:51:18 +0530

    create new test case for journal

--- server/tests/journal.test.js
+++ server/tests/journal.test.js
@@ -1,14 +1,13 @@
 import request from "supertest"
 import app from "../src/app.js"
-import mongoose from "mongoose";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 import { connectDB } from "../src/config/db.js";
 
 dotenv.config({ path: ".env.test", override: true });
 
 
-const userEmail = "akshaimuraleedharan@gmail.com"
-
+const userEmail = "akshaimuraleedharan8@gmail.com"
 
 
 beforeAll(async () => {
@@ -29,21 +28,9 @@ afterAll(async () => {
 
 let token = null
 
+describe("journal", () => {
 
-
-
-describe("Test server route health", () => {
-    test("health", async () => {
-        const res = await request(app).get("/api/health")
-        expect(res.statusCode).toBe(200);
-    })
-
-})
-
-describe("User Auth db test", () => {
-
-
-    test("Account Register", async () => {
+    test("Account Register for journal", async () => {
         const res = await request(app).post("/api/v1/user/register").send({
             userName: "akshai",
             email: userEmail,
@@ -52,16 +39,7 @@ describe("User Auth db test", () => {
         expect(res.statusCode).toBe(201);
     })
 
-    test("Account already exist", async () => {
-        const res = await request(app).post("/api/v1/user/register").send({
-            userName: "akshai",
-            email: userEmail,
-            password: "Akshai984"
-        })
-        expect(res.statusCode).toBe(409);
-    })
-
-    test("User Login", async () => {
+    test("User Login for journal", async () => {
 
         const res = await request(app).post("/api/v1/user/login").send({
             email: userEmail,
@@ -80,9 +58,22 @@ describe("User Auth db test", () => {
 
     })
 
+    test('create journal', async () => {
+        const res = await request(app).post("/api/v1/journal").set("Authorization", `Bearer ${token}`).send({
+            title: "BankNifty Breakout",
+            tradeDate: "2026-03-27T09:30:00Z",
+            tradeAmount: 10000,
+            pnl: 1250,
+            duration: "00:45",
+            timeFrame: "5m",
+            notes: "Breakout above resistance level",
+            strategy: "INTRADAY",
+            isProfit: "PROFIT"
+        })
+
+        expect(res.statusCode).toBe(201)
+        expect(res.body.success).toBe(true)
+        expect(res.body.message).toBe("Journal create successfully")
+
+    })
 })
-
-
-
-
-
